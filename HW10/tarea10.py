3869b74a47877c6a81739ae5d9ae7e524c4bdde7
ANDRES FELIPE GARCIA ALBARRACIN <af.garcia1214@compufi10.uniandes.edu.co>
2013-05-09 14:34:48 -0500

    Header

---
+++ HW10/tarea10.py
@@ -0,0 +1,18 @@
+# ******************************************************************
+# ******************** Universidad de los Andes ********************
+# ********************   Fisica computacional   ********************
+# ********************        Tarea 10          ********************
+# ******************************************************************
+# ***************   Andres Felipe Garcia Albarracin  ***************
+# ***************         Andrea Rozo Mendez	     ***************
+# ****************************************************************** 
+
+# Librerias
+import os, sys
+import numpy as np
+import matplotlib.pyplot as plt
+import pylab
+import scipy
+from scipy.fftpack import fft, fftfreq
+
+
